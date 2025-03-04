0867fb4d03d24a0a332bad04cfbdf7885d70d510
C3rb3rux-Code <polcrepal@campus.monlau.com>
2025-03-04 12:21:51 +0100

    title section fix

--- lib/title_section.dart
+++ lib/title_section.dart
@@ -10,12 +10,16 @@ class TitleSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Padding(
       padding: const EdgeInsets.all(12.0),
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            //crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 width: 150,
@@ -31,10 +35,11 @@ class TitleSection extends StatelessWidget {
               )
             ],
           ),
-          const SizedBox(width: 115), // Separador entre el título y el subtítulo
+          SizedBox(width: screenWidth * 0.29), // Separador entre el título y el subtítulo
           GestureDetector(
-            onTap: onSubtitleTap, // Acción cuando se toca el subtítulo
-            child: Text(
+            onTap: onSubtitleTap,
+            child:
+            Text(
               subtitle,
               style: const TextStyle(
                 color: Color.fromRGBO(0, 82, 158, 1),
